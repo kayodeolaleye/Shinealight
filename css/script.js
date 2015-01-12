dec0b89d479549ec63947a2afc9e64c7dcf5c352
kayodeolaleye <kaykola.olaleye@gmail.com>
2015-01-12 13:40:32 +0000

    colors
    on the colors...not done

--- css/script.js
+++ css/script.js
@@ -6,7 +6,7 @@ var menuList = menu.find('ul:first');
 var listItems = menu.find('li').not('#responsive-tab');
 
 // Create responsive trigger
-menuList.prepend('<li id="responsive-tab"><a href="#">Menu</a></li>');
+menuList.prepend('<li id="responsive-tab"><a href="#" style="">Menu</a></li>');
 
 // Toggle menu visibility
 menu.on('click', '#responsive-tab', function(){
